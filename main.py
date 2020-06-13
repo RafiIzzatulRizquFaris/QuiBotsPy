d5c0853d5296d7f60a747210f88425a76fe9d25e
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-13 18:40:56 +0700

    create checkout command message handler

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ cursor = connector.cursor()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi! \n To look the entire room that we have type /room \n")
+    bot.reply_to(message,
+                 "Hi! \n To look the entire room that we have type /room \n You want to reserve some room? just type /reservation #room_number#name#telephone#id_number \n Finished staying? type /checkout #room_number#year-month-day \n")
 
 
 @bot.message_handler(commands=['reservation'], func=lambda message: True)
@@ -27,10 +28,47 @@ def echo_all(message):
     data_customer_str = data_customer_str.replace('(', '')
     data_customer_str = data_customer_str.replace(')', '')
     data_customer_str = data_customer_str.replace(',', '')
+    cursor.execute("SELECT kamar.id_kamar FROM `kamar` WHERE `nomor_kamar` = " + room_number)
+    data_room = cursor.fetchone()
+    data_room_str = str(data_room)
+    data_room_str = data_room_str.replace('(', '')
+    data_room_str = data_room_str.replace(')', '')
+    data_room_str = data_room_str.replace(',', '')
     cursor.execute(
-        "INSERT INTO `reservasi` (`id_reservasi`, `id_pelanggan`, `id_kamar`, `tgl_checkin`) VALUES (NULL, '" + data_customer_str.strip() + "', '" + idKamar + "', CURRENT_DATE());")
+        "INSERT INTO `reservasi` (`id_reservasi`, `id_pelanggan`, `id_kamar`, `tgl_checkin`) VALUES (NULL, '" + data_customer_str.strip() + "', '" + data_room_str.strip() + "', CURRENT_DATE());")
     connector.commit()
     bot.reply_to(message, data_customer_str.strip())
+
+
+@bot.message_handler(commands=['checkout'], func=lambda message: True)
+def send_welcome(message):
+    order = message.text.split('#')
+    room_number = order[1]
+    checkout_date = order[2]
+    print(room_number)
+    cursor.execute("SELECT kamar.id_kamar FROM `kamar` WHERE `nomor_kamar` = " + room_number)
+    data_room = cursor.fetchone()
+    data_room_str = str(data_room)
+    data_room_str = data_room_str.replace('(', '')
+    data_room_str = data_room_str.replace(')', '')
+    data_room_str = data_room_str.replace(',', '')
+    cursor.execute("SELECT reservasi.id_reservasi FROM `reservasi` WHERE `id_kamar` = " + data_room_str.strip())
+    id_reservasi = cursor.fetchone()
+    id_reservasi_str = str(id_reservasi)
+    id_reservasi_str = id_reservasi_str.replace('(', '')
+    id_reservasi_str = id_reservasi_str.replace(')', '')
+    id_reservasi_str = id_reservasi_str.replace(',', '')
+    cursor.execute("SELECT reservasi.id_pelanggan FROM `reservasi` WHERE `id_kamar` = " + data_room_str.strip())
+    id_pelanggan = cursor.fetchone()
+    id_pelanggan_str = str(id_pelanggan)
+    id_pelanggan_str = id_pelanggan_str.replace('(', '')
+    id_pelanggan_str = id_pelanggan_str.replace(')', '')
+    id_pelanggan_str = id_pelanggan_str.replace(',', '')
+
+    cursor.execute("INSERT INTO `roomcheckout` (`id_checkout`, `id_reservasi`, `id_pelanggan`, `id_kamar`, `tgl_checkout`) VALUES (NULL, '"+id_reservasi_str.strip()+"', '"+id_pelanggan_str.strip()+"', '"+data_room_str.strip()+"', '"+checkout_date+"');")
+    connector.commit()
+
+    bot.reply_to(message, data_room_str.strip())
 
 
 @bot.message_handler(commands=['room'])
